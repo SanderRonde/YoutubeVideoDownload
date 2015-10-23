b0946358cdeed8aaa685e724352d0bc28e3936b9
Sander Ronde <awsdfgvhbjn@gmail.com>
2015-10-23 19:08:21 +0200

    Updated manifest, replaced " with '

--- YoutubeVideoDownload/YoutubeVideoDownload/app/background.js
+++ YoutubeVideoDownload/YoutubeVideoDownload/app/background.js
@@ -1,7 +1,7 @@
 var mp3list = [];
 chrome.downloads.onChanged.addListener(function (download) {
 	if (download.state) {
-		if (download.state.current === "complete") {
+		if (download.state.current === 'complete') {
 			for (var i = 0; i < mp3list.length; i++) {
 				if (mp3list[i] === download.id) {
 					chrome.downloads.erase({
@@ -19,23 +19,23 @@ chrome.runtime.onMessage.addListener(
 	function (request) {
 		if (localStorage.location === undefined && localStorage.location !== '') {
 			chrome.downloads.showDefaultFolder();
-			var loc = prompt("Please enter a storage location relative to your download location, the default location is the folder that just opened", "");
-			loc = loc.replace(/\\/g, "\\\\");
-			localStorage.setItem("location", loc);
+			var loc = prompt('Please enter a storage location relative to your download location, the default location is the folder that just opened', '');
+			loc = loc.replace(/\\/g, '\\\\');
+			localStorage.setItem('location', loc);
 		}
 
 		mp3list.push(request.data);
 
 		request.title = request.title
-			.replace(/&amp;/g, "&")
-			.replace("[//\\]/g", "-")
+			.replace(/&amp;/g, '&')
+			.replace('[//\\]/g', '-')
 			.replace(/[*:|]/g, '')
-			.replace(/["<>]/g, "'");
+			.replace(/["<>]/g, '\'');
 
 		console.log(request.data);
 		chrome.downloads.download({
 			url: request.data,
-			filename: localStorage.location + request.title + ".mp3"
+			filename: localStorage.location + request.title + '.mp3'
 		}, function (downloadItem) {
 			mp3list.push(downloadItem);
 		});
--- YoutubeVideoDownload/YoutubeVideoDownload/app/contentscript.js
+++ YoutubeVideoDownload/YoutubeVideoDownload/app/contentscript.js
@@ -1,18 +1,18 @@
-if (document.URL.search("youtube.com/watch") > -1) {
-	$("<div class='ytp-menuitem' aria-haspopup='false' tabindex='38' role='menuitem' href='#'>" +
-			"<div class='ytp-menuitem-label'>Download MP3</div>" +
-			"<div class='ytp-menuitem-content'></div>" +
-			"</div>")
+if (document.URL.search('youtube.com/watch') > -1) {
+	$('<div class=\'ytp-menuitem\' aria-haspopup=\'false\' tabindex=\'38\' role=\'menuitem\' href=\'#\'>' +
+			'<div class=\'ytp-menuitem-label\'>Download MP3 DA</div>' +
+			'<div class=\'ytp-menuitem-content\'></div>' +
+			'</div>')
 		.click(function() {
-			var newwindow = window.open("http://www.youtube-mp3.org/#v" + document.URL.split("watch?v=")[1]);
+			var newwindow = window.open('http://www.youtube-mp3.org/#v' + document.URL.split('watch?v=')[1]);
 			newwindow.focus();
 		})
-		.appendTo("#contextmenu");
+		.appendTo('#contextmenu');
 }
-else if (document.URL.search("youtube-mp3.org") > -1) {
-	$('#youtube-url').attr('value', 'http://www.youtube.com/watch?v=' + location.href.split("#v")[1]);
+else if (document.URL.search('youtube-mp3.org') > -1) {
+	$('#youtube-url').attr('value', 'http://www.youtube.com/watch?v=' + location.href.split('#v')[1]);
 	setTimeout(function () {
-		$("#submit").click();
+		$('#submit').click();
 	}, 50);
 	var progressInfoEl = $('#progress_info')[0];
 	var timer = window.setInterval(function () {
@@ -20,8 +20,8 @@ else if (document.URL.search("youtube-mp3.org") > -1) {
 			var visibleEl = $('#dl_link').children().not('[style]')[0];
 			if (visibleEl) {
 				chrome.runtime.sendMessage({
-					data: 'http://www.youtube-mp3.org' + visibleEl.getAttribute("href"),
-					title: document.getElementById("title").innerHTML.split("</b> ")[1]
+					data: 'http://www.youtube-mp3.org' + visibleEl.getAttribute('href'),
+					title: document.getElementById('title').innerHTML.split('</b> ')[1]
 				});
 				window.close();
 				window.clearInterval(timer);
@@ -29,17 +29,17 @@ else if (document.URL.search("youtube-mp3.org") > -1) {
 		}
 	}, 150);
 }
-else if (document.URL.search("youtube.com/results") > -1) {
+else if (document.URL.search('youtube.com/results') > -1) {
 
 	var justdownloaded = false;
 
 	var $ytLockupThumbnail = $('.yt-lockup-thumbnail').mouseenter(function() {
-		$(this).children(".download-mp3-button").show();
+		$(this).children('.download-mp3-button').show();
 	}).mouseleave(function() {
-		$(this).children(".download-mp3-button").hide();
+		$(this).children('.download-mp3-button').hide();
 	});
 	var $buttonContent;
-	var $buttonCont = $('<button class="yt-uix-button yt-uix-button-size-small yt-uix-button-default yt-uix-button-empty yt-uix-button-has-icon no-icon-markup video-actions spf-nolink yt-uix-tooltip download-mp3-button hide-until-delayloaded" type="button" onclick=";return false;" title="Download MP3" role="button" data-video-ids="lgbW6IuwcZQ" data-tooltip-text="Download MP3"></button>')
+	var $buttonCont = $('<button class="yt-uix-button yt-uix-button-size-small yt-uix-button-default yt-uix-button-empty yt-uix-button-has-icon no-icon-markup video-actions spf-nolink yt-uix-tooltip download-mp3-button hide-until-delayloaded" type="button" onclick=";return false;" title="Download MP3 DA" role="button" data-video-ids="lgbW6IuwcZQ" data-tooltip-text="Download MP3 DA"></button>')
 		.css({
 			marginRight: '24px',
 			padding: 0,
@@ -47,23 +47,23 @@ else if (document.URL.search("youtube.com/results") > -1) {
 			height: '22px'
 		})
 		.click(function() {
-			$buttonContent.css("background", "no-repeat url(//s.ytimg.com/yts/imgbin/www-hitchhiker-vfl8VEEGb.webp) -51px -725px");
-			$(this).css("background-color", "green");
+			$buttonContent.css('background', 'no-repeat url(//s.ytimg.com/yts/imgbin/www-hitchhiker-vfl8VEEGb.webp) -51px -725px');
+			$(this).css('background-color', 'green');
 			var _this = $(this);
 			justdownloaded = true;
 			setTimeout(function() {
 				justdownloaded = false;
-				_this.css("background-color", "rgb(248,248,248)");
-				$buttonContent.css("background", "no-repeat url(//s.ytimg.com/yts/imgbin/www-hitchhiker-vfl8VEEGb.webp) -1px -700px");
+				_this.css('background-color', 'rgb(248,248,248)');
+				$buttonContent.css('background', 'no-repeat url(//s.ytimg.com/yts/imgbin/www-hitchhiker-vfl8VEEGb.webp) -1px -700px');
 			}, 5000);
 
-			var newwindow = window.open("http://www.youtube-mp3.org/#v" + $(this).parent().parent().children(".yt-lockup-thumbnail").children("a").attr("href").split("watch?v=")[1]);
+			var newwindow = window.open('http://www.youtube-mp3.org/#v' + $(this).parent().parent().children('.yt-lockup-thumbnail').children('a').attr('href').split('watch?v=')[1]);
 			newwindow.focus();
 		})
 		.hide()
 		.appendTo($ytLockupThumbnail);
 
-	$buttonContent = $("<div></div>")
+	$buttonContent = $('<div></div>')
 		.css({
 			background: 'no-repeat url(//s.ytimg.com/yts/imgbin/www-hitchhiker-vfl8VEEGb.webp) -1px -700px',
 			backgroundSize: 'auto',
@@ -73,10 +73,10 @@ else if (document.URL.search("youtube.com/results") > -1) {
 			marginTop: '3px'
 		})
 		.mouseenter(function() {
-			$(this).css("background", "no-repeat url(//s.ytimg.com/yts/imgbin/www-hitchhiker-vfl8VEEGb.webp) -1px -724px");
+			$(this).css('background', 'no-repeat url(//s.ytimg.com/yts/imgbin/www-hitchhiker-vfl8VEEGb.webp) -1px -724px');
 		}).mouseleave(function() {
 			if (!justdownloaded) {
-				$(this).css("background", "no-repeat url(//s.ytimg.com/yts/imgbin/www-hitchhiker-vfl8VEEGb.webp) -1px -700px");
+				$(this).css('background', 'no-repeat url(//s.ytimg.com/yts/imgbin/www-hitchhiker-vfl8VEEGb.webp) -1px -700px');
 			}
 		})
 		.appendTo($buttonCont);
